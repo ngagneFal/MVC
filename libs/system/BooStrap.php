38a7875f0886333ef5e3b2817f8bfe1510268222
Ngagne <ngagne0303@gmail.com>
2021-03-04 14:27:00 +0000

    add

--- libs/system/BooStrap.php
+++ libs/system/BooStrap.php
@@ -4,9 +4,12 @@ class BooStrap
 {
     public function __construct()
     {
+        
         if (isset($_GET["url"]))
         {
-            echo $_GET["url"];
+            echo 12;
+            //echo $_GET["url"];
+
         }else {
             echo "MVC";
         }
